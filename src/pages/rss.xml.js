5be10903efc3ec1275e26bfb9fe0de350bb813c4
OppsBadass <qingmohh@163.com>
2023-03-21 16:53:19 +0800

    feat: add rss xml

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+
+export async function get() {
+  return rss({
+    title: "Astro Learner | Blog",
+    description: "My journey learning Astro",
+    site: "http://m-astro-blog.netlify.app",
+    item: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    customData: `<lanuage>en-us</lanuage>`,
+  });
+}
